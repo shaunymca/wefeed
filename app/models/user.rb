2334f95de61e186bfce114cfb4c411e775abead5
smcavinney <shaun.mcavinney@gmail.com>
2013-11-23 03:43:37 +0000

    relation between user and post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   rolify
   has_many :authentications
+  has_many :posts
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable

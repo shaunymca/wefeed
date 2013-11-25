39506e6d8c62c116ba326eaaaf1505c21b265de1
smcavinney <shaun.mcavinney@gmail.com>
2013-11-25 22:56:38 +0000

    Iterated on friendships a bit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @users = User.all
+    @nonfriends = User.all.where.not(id: current_user.friend_ids).where.not(id: current_user.id)
+    @users = User.all.where(id: current_user.friend_ids)
+    
   end
 
   def show
--- app/views/friendships/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @friendship, :user_id, :friend_id, :created_at, :updated_at

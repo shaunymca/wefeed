6b6d01b6dd4ee5a936424e53d0e0ab4a3395db15
smcavinney <shaun.mcavinney@gmail.com>
2013-11-26 04:44:02 +0000

    added in user followers porfile logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,17 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @nonfriends = User.all.where.not(id: current_user.friend_ids).where.not(id: current_user.id)
-    @users = User.all.where(id: current_user.friend_ids)
-    
+    @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+    @followers = @user.inverse_friendships.all
+    @followed = @user.friendships.all
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
 end

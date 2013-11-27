163067c96d46e347181e880e03bd24b2e0c22265
smcavinney <shaun.mcavinney@gmail.com>
2013-11-27 20:19:48 +0000

    posts form working on posts page

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,25 @@
 Wefeed::Application.routes.draw do
-
+  
   resources :reposts
-
+  
   resources :friendships
-
+  resources :home
   resources :posts
   resources :activities
-
+    
+  authenticated :user do
+    root :to => "posts#index"
+  end
+  
+  devise_scope :user do
+    get "/" => "home#index"
+  end
   #match '/auth/:provider/callback' => 'authentications#create'
   resources :authentications
   
   devise_for :users, path_names: {sign_in: "login", sign_out: "sign_out"}, controllers: {omniauth_callbacks: "authentications", registrations: "registrations"}
   resources :users
 
-  root to: 'authentications#home'
+
+  
 end

e38866732cfbc3a61a37ae14cfb5cfa6743e75b2
smcavinney <shaun.mcavinney@gmail.com>
2013-12-05 22:10:44 +0000

    removed the repost updating post updated_at field

--- app/models/repost.rb
+++ app/models/repost.rb
@@ -4,8 +4,4 @@ class Repost < ActiveRecord::Base
   accepts_nested_attributes_for :post
   after_save :update_post
   
-  def update_post
-    post.updated_at = self.created_at
-    post.save
-  end
 end

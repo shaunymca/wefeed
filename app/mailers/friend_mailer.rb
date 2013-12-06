b39894bf5a498b71369cb9b4b774582b30c89d08
smcavinney <shaun.mcavinney@gmail.com>
2013-12-06 01:33:22 +0000

    fixed email, again

--- app/mailers/friend_mailer.rb
+++ app/mailers/friend_mailer.rb
@@ -5,7 +5,7 @@ class FriendMailer < ActionMailer::Base
     @user = followed
     @follower = follower
     @user_url  = 'http://wefeed.co/users/'+@user.id.to_s
-    @follwer_url  = 'http://wefeed.co/users/'+ @follower.id.to_s
+    @follower_url  = 'http://wefeed.co/users/'+ @follower.id.to_s
     mail(to: @user.email, subject: "#{@follower.name} Followed you on WeFeed")
   end
 end

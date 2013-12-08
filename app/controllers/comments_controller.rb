1ce5ae2953b099d2ff5fd708d2fecfb1e8579aec
smcavinney <shaun.mcavinney@gmail.com>
2013-12-08 02:12:50 +0000

    got remote comments to work in-line with markdown.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,15 +15,24 @@ class CommentsController < ApplicationController
   end
   
   def update
-    @post = Post.find(params[:post_id])
+    #@post = Post.find(params[:post_id])
     @comment = Comment.find params[:id]
     
     respond_to do |format|
-      if @comment.update_attributes(params[:comment])
+      if @comment.update_attributes(params[:comment].permit(:content))
         format.json { respond_with_bip(@comment) }
       else
         format.json { respond_with_bip(@comment) }
       end
+    end
+  end
+  
+  def destroy
+    @comment = Comment.destroy(params[:id])
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url }
+      format.js
     end
   end
   
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,15 +2,14 @@ class Comment < ActiveRecord::Base
   include PublicActivity::Model
   belongs_to :post
   belongs_to :user
-  before_save :markdown_convert
-  
-  def markdown_convert
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true, :no_intra_emphasis => true, :footnotes => true)
-    self.content = markdown.render(self.content.to_s).to_s
-  end
   
   def user_name
     self.user.name
+  end
+  
+  def comment_mk
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true, :no_intra_emphasis => true, :footnotes => true)
+    markdown.render(self.content.to_s).html_safe
   end
   
 end
